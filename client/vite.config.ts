06205e81d10bee931debeaa5aa4bba56bd18d569
matdombrock <dombrock.mathieu@gmail.com>
2026-07-20 18:35:43 -0700

    host

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 export default defineConfig({
   plugins: [svelte()],
   server: {
+    host: true,
     port: 5173,
     proxy: {
       '/api': {
